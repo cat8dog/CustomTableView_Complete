d428a3792300490bc2f6e721679406a267ccf2a5
cat8dog <catherine.reyto@gmail.com>
2015-04-14 17:00:44 -0400

    git init

---
+++ CutsomTable_practice/CustomTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.h
+//  CutsomTable_practice
+//
+//  Created by Catherine Reyto on 2015-04-14.
+//  Copyright (c) 2015 Catherine Reyto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell
+
+
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *prepTimeLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *thumbnailImageView;
+
+
+@end
